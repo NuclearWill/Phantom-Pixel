0ca42874697868d5498325bbdbf14a4f910379a6
Will Thomsen <willthomsen@hotmail.com>
2025-04-27 13:13:52 -0400

    minor updates

--- Phantom Pixel/Assets/Scripts/LevelManager.cs
+++ Phantom Pixel/Assets/Scripts/LevelManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public static class LevelManager
 {
@@ -8,11 +10,44 @@ public static class LevelManager
     public static void completeLevel(int level)
     {
         levelCompleted[level - 1] = true;
+        SaveLevel();
     }
 
     public static void LoseLevel()
     {
         Debug.Log("You lost the level!");
         TimeManager.RestartTime();
+    }
+
+    public static void SaveLevel()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/Momentary.SaveData";
+        FileStream stream = new FileStream(path, FileMode.Create);
+        // This is probably me worrying again but im not sure if this "safe"
+        // I'm not sure if this handles if there is already data written at that point
+        // bu this is a tutorial so let me wait and see
+        
+        formatter.Serialize(stream, levelCompleted);
+        stream.Close();
+
+    }
+
+    public static void LoadLevel()
+    {
+        string path = Application.persistentDataPath + "/Momentary.SaveData";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            levelCompleted = formatter.Deserialize(stream) as bool[];
+            stream.Close();
+            
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+        }
     }
 }
--- Phantom Pixel/Assets/Scripts/Save and level load/Portal unlock.cs
+++ Phantom Pixel/Assets/Scripts/Save and level load/Portal unlock.cs
@@ -3,7 +3,17 @@ using System.IO;
 
 public class Portalunlock : MonoBehaviour
 {
+   
    public int Level1 = 0;
    public int Level2 = 0;
    public int Level3 = 0;
+    
+   public bool isLocked;
+
+   public void getLevelState(int level)
+   {
+      
+   }
+    
+   
 }
